b6cbf292e1fe1460452b03d3a2df436b368d197a
NguyenDuc1003 <20223502@eaut.edu.vn>
2025-12-17 13:23:48 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        float f = 20.5f;
+        double d = 30.5;
+        Boolean b = true;
+        char c = 'A';
+        string s = "Hello, World!";
+        Console.WriteLine("Integer: " + i);
+        Console.WriteLine("Float:" + f);
+        Console.WriteLine("double:" + d);
+        Console.WriteLine("Boolean:" + b);
+        Console.WriteLine("Char:" + c);
+        Console.WriteLine("String:" + s);
+    }
+}
